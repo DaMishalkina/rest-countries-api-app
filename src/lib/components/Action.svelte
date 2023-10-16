70888f40070191e6ce9b76d00f4adf0c65e22a74
Daria Mishalkina <dashaio251@gmail.com>
2023-10-16 11:38:12 +0200

    add links

---
+++ src/lib/components/Action.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    export let link = "", title = "", isLarge = false;
+
+</script>
+
+
+<a
+        href={link}
+        class="action"
+        class:action--large={isLarge}
+>
+    <slot/>
+    {title}
+</a>
+
+<style>
+    .action {
+        display: flex;
+        padding: 20px;
+        justify-content: center;
+        align-items: center;
+        background: var(--elements-bg);
+        text-decoration: none;
+        color: var(--text-color);
+        box-shadow: rgba(100, 100, 111, 0.2) 0 7px 29px 0;
+        border: none;
+        border-radius: 3px;
+        font-size: 16px;
+        gap: 20px;
+        flex: 1;
+    }
+    .action--large {
+        padding: 20px 50px;
+        align-self: flex-start;
+    }
+    .action:hover {
+        transform: translateY(-3px);
+    }
+    .action:active, .action:focus {
+        transform: translateY(-1px);
+    }
+</style>
+
+
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,10 +1,53 @@
 <script lang="ts">
-    import type {MainCountry} from "$lib/types";
-    export let data: MainCountry;
+    import type {MainCountry, BorderCountry} from "$lib/types";
+    import Action from "$lib/components/Action.svelte";
+
+    export let data: {country: MainCountry, borders: BorderCountry[]};
 </script>
 
-<!--<h1>{data.slug.replace(/-/g, " ")}</h1>-->
+<main class="main">
+    <Action link="/" title="Back" isLarge>
+        <svg
+                width="22"
+                height="22"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                fill="currentColor"
+        >
+            <path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z"/>
+        </svg>
+    </Action>
+    <section>
+        <h1>{data?.country.name.common}</h1>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+        {#if data?.borders.length > 0}
+            <div class="country__borders">
+                {#each data?.borders as border (border.name.common)}
+                  <Action
+                          link="/{border.name.common.toLowerCase().replace(/\s/g,'-')}"
+                          title={border.name.common}
+                  />
+                {/each}
+            </div>
+        {/if}
+    </section>
+</main>
 
-<pre>{JSON.stringify(data, null, 2)}</pre>
+<style>
+    .main {
+        display: flex;
+        flex-direction: column;
+        padding: 80px 55px 120px 55px;
+    }
+    .country__borders {
+        display: flex;
+        align-items: stretch;
+        justify-content: space-between;
+        gap: 20px;
+        flex-wrap: wrap;
+    }
+</style>
+
 
 
